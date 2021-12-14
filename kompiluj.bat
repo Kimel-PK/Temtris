2823046be2944147dd3db7287d3764f167cdb9f6
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-12-14 20:31:26 +0100

    Modyfikacja skryptu kompilującego

--- kompiluj.bat
+++ kompiluj.bat
@@ -4,6 +4,3 @@ del temtris.nes
 ca65 temtris.asm -o temtris.o --debug-info
 ld65 temtris.o -o temtris.nes -t nes --dbgfile temtris.dbg
 temtris.nes
-IF EXIST "temtris.nes" GOTO dziala
-pause
-:dziala
